ec60ccd29fe28063e57d17441197d535d5cf2afb
kaemoney0147 <114649936+kaemoney0147@users.noreply.github.com>
2022-10-04 14:53:44 +0100

    coding in progress

---
+++ index.js
@@ -0,0 +1,16 @@
+// my first java assigment using variable
+let firstWord = 'Hello,'
+let firstName = 'Abiodun'
+let space = ' '
+let secondName= 'Akinlotan'
+let middleName= 'Kawole'
+console.log('....Welcome....')
+let addUp= firstWord + space + firstName
+let secondAddUp= middleName + space + secondName
+let totalAddUp= addUp + space+ secondAddUp
+console.log('Greetings:',totalAddUp)
+let age=33
+let currentYear=2022
+let birthYear=2022-33
+console.log('my birthyear:',birthYear)
+
